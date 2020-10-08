a88d220df86d24914d2818aef7a2034b0e756063
khelechy <onyekwerekelechimac@gmail.com>
2020-10-08 09:04:53 +0100

    initial commit

---
+++ cryptor-ui/Models/CrypterModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cryptor_ui.Models
+{
+	public class CrypterModel
+	{
+		public string message { get; set; }
+		public string key { get; set; }
+	}
+}
